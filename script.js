0d5ac4477e1e27834192f81298e6be0cb7de1c4a
emprendepues2021-spec <emprendepues2021@gmail.com>
2026-06-02 11:40:22 -0500

    more changes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function saludar() {
     alert("¡Hola! Soy Frank y esta es mi primera página interactiva 🚀");
 }
+function cambiarMensaje() {
+
+    document.getElementById("mensaje").innerHTML =
+        "🚀 Estoy aprendiendo programación en mi MacBook Air M1";
+
+}
