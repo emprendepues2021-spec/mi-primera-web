9ba730dae316a3f0523dfd263d90bb0953cba959
emprendepues2021-spec <emprendepues2021@gmail.com>
2026-06-02 12:00:38 -0500

    Agregar secciones de portafolio

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function cambiarMensaje() {
         "🚀 Estoy aprendiendo programación en mi MacBook Air M1";
 
 }
+let clicks = 0;
+
+function cambiarMensaje() {
+    document.getElementById("mensaje").innerHTML =
+        "🚀 Estoy aprendiendo programación en mi MacBook Air M1";
+}
+
+function contarClicks() {
+    clicks++;
+
+    document.getElementById("contador").innerHTML =
+        "Has hecho " + clicks + " clics.";
+}
